ccbc752b49f9fe2fbafa067b1017e47e3521d58a
unnato0159 <unnato01@naver.com>
2019-04-14 11:13:01 +0900

    JAVA 에서 findViewById 로 XML 의 컴포넌트 연결  - 멤버 변수 생성  - onCreate 에서 실제로 findVIewById 로 연결

--- app/src/main/java/com/tje/a20190414_java_xml/MainActivity.java
+++ app/src/main/java/com/tje/a20190414_java_xml/MainActivity.java
@@ -3,8 +3,18 @@ package com.tje.a20190414_java_xml;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView contentTextView;
+
+    EditText inputEditText;
+
+    Button okBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +23,10 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d("메인-onCreate테스트","정말실행되나");
         Log.e("메인-e로그","어떻게 나오나 ");
+
+        contentTextView = findViewById(R.id.contentTextView);
+        inputEditText = findViewById(R.id.inputEditText);
+        okBtn = findViewById(R.id.okBtn);
 
     }
 }
